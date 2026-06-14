6f863c1aaa443666ae5f6aba46f455e5f8a6be11
Assyrian91 <khoshabayalda91@gmail.com>
2026-06-14 18:13:43 +0300

    update requirements.txt

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,7 @@
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from run_etl import * 
+from forecasting.forecast import run_forecast
 
 import streamlit as st
 import pandas as pd
